37dc6a18052c15ff83817ca207ab5891939bc9cb
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-10-10 00:14:04 +0700

    Insert into [Activites]

---
+++ Insert Into Table.sql
@@ -0,0 +1,9 @@
+INSERT INTO [Activities]
+([sell_date], [product]) VALUES
+('2020-05-30', 'Headphone'),
+('2020-06-01', 'Pencil'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'Basketball'),
+('2020-06-01', 'Bible'),
+('2020-06-02', 'Mask'),
+('2020-05-30', 'T-Shirt')
