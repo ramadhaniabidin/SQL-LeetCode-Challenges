2d94e1a523465e8bc95030ad9e8c78c972f2c5df
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-11-03 00:53:20 +0700

    update

--- Create Table.sql
+++ Create Table.sql
@@ -1,4 +1,14 @@
-CREATE TABLE [dbo].[Activities](
-    [sell_date] DATE,
-    [product] VARCHAR(100)
+-- CREATE TABLE [dbo].[Activities](
+--     [sell_date] DATE,
+--     [product] VARCHAR(100)
+-- )
+
+
+CREATE TABLE [dbo].[Signups](
+    [user_id] INT,
+    [time_stamp] DATETIME
+)
+
+CREATE TABLE [Confirmations](
+    
 )
